fd3d5ca28ad7d8afca20ee5a4046ca89c7405baf
Vadim <vadim_kimm@icloud.com>
2022-09-04 19:18:04 +0300

    Create struct RadioTopSectionView

---
+++ AppleMusic/Sources/Scenes/Radio/View/RadioTopSectionView.swift
@@ -0,0 +1,58 @@
+//
+//  RadioTopSectionView.swift
+//  AppleMusic
+//
+//  Created by Vadim Kim on 04.09.2022.
+//
+
+import SwiftUI
+
+struct RadioTopSectionView: View {
+
+    let radioItem: RadioModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(radioItem.category?.rawValue ?? "Error")
+                .foregroundColor(.secondary)
+                .textCase(.uppercase)
+                .font(.caption)
+
+            Text(radioItem.title)
+                .foregroundColor(.primary)
+                .font(.title3)
+
+            Text(radioItem.subtitle)
+                .foregroundColor(.secondary)
+                .font(.caption)
+
+            ZStack(alignment: .bottomLeading) {
+                Image(radioItem.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(Metrics.imageCornerRadius)
+
+                if let description = radioItem.description {
+                    Text(description)
+                        .font(.caption)
+                        .padding(Metrics.padding)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        .padding(.horizontal, Metrics.padding)
+    }
+}
+
+struct RadioTopSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioTopSectionView(radioItem: RadioModel.getPreviewData()[0])
+    }
+}
+
+extension RadioTopSectionView {
+    enum Metrics {
+        static let imageCornerRadius: CGFloat = 10
+        static let padding: CGFloat = 8
+    }
+}

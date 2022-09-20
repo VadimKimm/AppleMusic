9ca07127f491cd9a6e850bc741ff8803768f6e72
Vadim <vadim_kimm@icloud.com>
2022-09-20 20:47:17 +0300

    Create struct PlayerDetailView

---
+++ AppleMusic/Sources/Scenes/Player/View/InnerViews/PlayerOptionButtonsView.swift
@@ -0,0 +1,34 @@
+//
+//  PlayerOptionButtonsView.swift
+//  AppleMusic
+//
+//  Created by Vadim Kim on 20.09.2022.
+//
+
+import SwiftUI
+
+struct PlayerOptionButtonsView: View {
+    var body: some View {
+        HStack(spacing: 60) {
+            createButton(imageName: "quote.bubble", message: "Lyrics button pressed")
+            createButton(imageName: "airplayaudio", message: "Airplay button pressed")
+            createButton(imageName: "list.bullet", message: "Playlist button pressed")
+        }
+    }
+
+    private func createButton(imageName: String, message: String) -> some View {
+        return Button {
+            print(message)
+        } label: {
+            Image(systemName: imageName)
+                .accentColor(Color.gray.opacity(0.8))
+                .font(.title2)
+        }
+    }
+}
+
+struct PlayerOptionButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerOptionButtonsView()
+    }
+}

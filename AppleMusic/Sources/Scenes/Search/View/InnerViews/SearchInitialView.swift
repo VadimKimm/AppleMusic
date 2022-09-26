6ea513e52f159a4e47205f3996d8f4a085a7074f
Vadim <vadim_kimm@icloud.com>
2022-09-26 19:35:19 +0300

    Make struct SearchView lighter

---
+++ AppleMusic/Sources/Scenes/Search/View/InnerViews/SearchInitialView.swift
@@ -0,0 +1,56 @@
+//
+//  SearchInitialView.swift
+//  AppleMusic
+//
+//  Created by Vadim Kim on 26.09.2022.
+//
+
+import SwiftUI
+
+struct SearchInitialView: View {
+
+    @State var model: ModelData
+
+    let columns = [
+        GridItem(.flexible(), spacing: Metrics.columnsSpacing),
+        GridItem(.flexible(), spacing: Metrics.columnsSpacing)
+    ]
+
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Divider()
+
+                Text("Поиск по категориям")
+                    .font(.title2)
+                    .bold()
+
+                LazyVGrid(columns: columns) {
+                    ForEach(model.searchCategories) { item in
+                        NavigationLink {
+                            SearchDetailView(model: model, title: item.title)
+                        } label: {
+                            SearchCategoryView(categoryItem: item)
+                        }
+                    }
+                    .frame(width: Metrics.frameWidth, height: Metrics.frameHeight)
+                }
+            }
+            .padding(.horizontal, Metrics.padding)
+
+            Spacer(minLength: Metrics.spacerMinLength)
+        }
+        .navigationTitle("Поиск")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+extension SearchInitialView {
+    enum Metrics {
+        static let padding: CGFloat = 15
+        static let frameWidth: CGFloat = UIScreen.main.bounds.width / 2
+        static let frameHeight: CGFloat = UIScreen.main.bounds.width / 3
+        static let spacerMinLength: CGFloat = 80
+        static let columnsSpacing: CGFloat = 10
+    }
+}
--- AppleMusic/Sources/Scenes/Search/View/SearchView.swift
+++ AppleMusic/Sources/Scenes/Search/View/SearchView.swift
@@ -15,67 +15,18 @@ struct SearchView: View {
     @State private var selectedSearchTag = 0
     @State private var searchPlaceholder = "Ваша медиатека"
 
-    let columns = [
-        GridItem(.flexible(), spacing: Metrics.columnsSpacing),
-        GridItem(.flexible(), spacing: Metrics.columnsSpacing)
-    ]
-    
-
     var body: some View {
         NavigationView {
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(alignment: .leading) {
-                    Divider()
-
-                    Text("Поиск по категориям")
-                        .font(.title2)
-                        .bold()
-
-                    LazyVGrid(columns: columns) {
-                        ForEach(model.searchCategories) { item in
-                            NavigationLink {
-                                SearchDetailView(model: model, title: item.title)
-                            } label: {
-                                SearchCategoryView(categoryItem: item)
-                            }
-                        }
-                        .frame(width: Metrics.frameWidth, height: Metrics.frameHeight)
-                    }
-                }
-                .padding(.horizontal, Metrics.padding)
-
-                Spacer(minLength: Metrics.spacerMinLength)
-            }
-            .navigationTitle("Поиск")
-            .navigationBarTitleDisplayMode(.large)
-            .searchable(text: $searchText,
-                        placement: .navigationBarDrawer(displayMode: .always),
-                        prompt: $searchPlaceholder.wrappedValue) {
-
-                VStack(alignment: .leading) {
-                    Picker("SearchSource", selection: $selectedSearchTag) {
-                        Text("Apple Music").tag(0)
-                        Text("Ваша медиатека").tag(1)
-                    }
-                    .onChange(of: selectedSearchTag) { tag in
-                        if tag == 0 {
-                            searchPlaceholder = "Артисты, песни, тексты и др."
-                        } else {
-                            searchPlaceholder = "Ваша медиатека"
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                }
-
-                if searchText.isEmpty {
-                    ForEach(model.tracks) { result in
-                        SearchTrackView(track: result) }
-                } else {
-                    ForEach(model.tracks.filter { $0.song.contains(searchText) || $0.author.contains(searchText)  }) { result in
-                        SearchTrackView(track: result)
-                    }
-                }
-            }
+            SearchInitialView(model: model)
+        }
+        .searchable(text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always),
+                    prompt: $searchPlaceholder.wrappedValue) {
+            
+            SearchPickerView(selectedSearchTag: $selectedSearchTag,
+                             searchText: $searchText,
+                             searchPlaceholder: $searchPlaceholder,
+                             model: model)
         }
         .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
             isSearchBarActive = true
@@ -90,15 +41,5 @@ struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView(isSearchBarActive: .constant(false))
             .previewInterfaceOrientation(.portrait)
-    }
-}
-
-extension SearchView {
-    enum Metrics {
-        static let padding: CGFloat = 15
-        static let frameWidth: CGFloat = UIScreen.main.bounds.width / 2
-        static let frameHeight: CGFloat = UIScreen.main.bounds.width / 3
-        static let spacerMinLength: CGFloat = 80
-        static let columnsSpacing: CGFloat = 10
     }
 }
